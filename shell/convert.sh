3fd3e58fd56c0244c2b7e7a8c3462913617dfc14
DESKTOP-45A1PAS\SAMLOU <samloucn@outlook.com>
2026-08-20 18:59:44 +0800

    added config.yaml to the shell/ and fixed issue of convert.sh

--- shell/convert.sh
+++ shell/convert.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "This tool will help you convert .pt files to .bin files"
-if [[ ! -d "../docker" ]];then
-	mkdir ../docker
-	mkdir ../docker/my_workspace
-fi
+mkdir -p ../docker/my_workspace
 if [[ ! -d "../samples" ]];then
 	mkdir ../samples
 fi
@@ -25,24 +22,29 @@ if [[ ! -f "../docker/horizon_x5_open_explorer_v1.2.8-py310_20240926.tar.gz" ]];
 		exit 1
 	fi
 fi
-if [[ ! -f "../samples/rdk_model_zoo" ]];then
+if [[ ! -d "../samples/rdk_model_zoo" ]];then
 	echo "missing MODEL ZOO..."
 	if git clone https://github.com/D-Robotics/rdk_model_zoo.git ../samples;then
 		echo "clone success...."
-		git checkout rdk_x5
+		git -C ../samples/rdk_model_zoo checkout rdk_x5
 	else
 		echo "[ERROR]clone failed!"
 		exit 1
 	fi
 fi
+if [[ ! -f "$1" ]]; then 
+	echo "[ERROR]file not existed!"
+	exit 1
+fi
 if [[ "$1" == *.pt ]];then
 	echo ".pt file loaded"
 else
 	echo "[ERROR]it's not a .pt file!"
+	exit 1
 fi
 python3 -m venv .tmp_venv
-.tmp_venv/bin/python ../samples/rdk_model_zoo/samples/vision/ultralytics_yolo/conversion/export_monkey_patch.py --pt $1
 .tmp_venv/bin/pip install ultralytics
+.tmp_venv/bin/python ../samples/rdk_model_zoo/samples/vision/ultralytics_yolo/conversion/export_monkey_patch.py --pt $1
 rm -rf .tmp_venv
 dir=$(dirname "$1")
 basename=$(basename "$1" .pt)
@@ -51,11 +53,15 @@ if [[ ! -f "$onnx_file" ]];then
 	echo "[ERROR].onnx file not found!"
 	exit 1
 else
-	cp $onnxfile ../docker/my_workspace
+	cp $onnx_file ../docker/my_workspace
 fi
 cp config.yaml ../docker/my_workspace
 cp -r ../pictures ../docker/my_workspace
-if docker run -it --rm -v $(pwd)/../docker/horizon_x5_open_explorer_v1.2.8-py310_20240926.tar.gz:/open_explorer -v $(pwd)/my_workspace:/workspace openexplorer/ai_toolchain_ubuntu_20_x5_cpu:v1.2.8-py310 /bin/bash -c "hb_mapper makertbin --config ../workspace/my_workspace/config.yaml --model-type onnxS"; then
+tar -xf ../docker/horizon_x5_open_explorer_v1.2.8-py310_20240926.tar.gz -C ../docker/
+if docker run -it --rm -v $(pwd)/../docker/horizon_x5_open_explorer_v1.2.8-py310_20240926:/open_explorer \
+-v $(pwd)/../docker:/workspace openexplorer/ai_toolchain_ubuntu_20_x5_cpu:v1.2.8-py310 /bin/bash \
+-c "hb_mapper makertbin --config ../workspace/my_workspace/config.yaml \
+--model-type onnx"; then
 	echo "docker run successful"
 else
 	echo "[ERROR]docker run failed, code: $?"
